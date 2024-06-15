f0a13c03e1440f8863ae1e4992eaeaa11156342c
AnanSer <ananserbesa@gmial.com>
2024-06-15 12:17:03 +0300

    added cards.js and cards.css

--- src/App.js
+++ src/App.js
@@ -30,18 +30,17 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
-      ></ExpenseItem>
-      {/* <ExpenseItem></ExpenseItem> */}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <p className="expense-item__price">{props.amount}</p>
       </div>
-    </div>
+    </Card>
   );
 }
 

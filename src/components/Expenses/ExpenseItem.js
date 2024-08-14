10a39381dd89707fcc1ccb2b9c1be04e422e42f9
AnanSer <ananserbesa@gmail.com>
2024-08-14 12:58:21 +0300

    some modification on the code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,21 +4,22 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
+  /*  const [title, setTitle] = useState(props.title);
 
   const ClickHundler = () => {
     setTitle("Updated");
     console.log({ title });
   };
+*/
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <p className="expense-item__price">{props.amount}</p>
+        <h2>{props.title}</h2>
+        <p className="expense-item__price">{props.amount} birr</p>
       </div>
-      <button onClick={ClickHundler}>Change Title</button>
+      {/* <button onClick={ClickHundler}>Change Title</button> */}
     </Card>
   );
 };
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,15 +1,22 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2024");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter
-        // selected={filteredYear}
-        // onChangeFilter={filteredChangeHandler}
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
 
         {props.items.map((expense) => (
@@ -20,7 +27,7 @@ const Expenses = (props) => {
             date={expense.date}
           />
         ))}
-        {/* 
+        {/*         
         <ExpenseItem            // these are static way 
           title={props.items[0].title}
           amount={props.items[0].amount}
